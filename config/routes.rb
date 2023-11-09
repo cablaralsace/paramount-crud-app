a1474e598911d04f7d510c8ca178d95dfe0b029d
Alsace Cablar <cablaralsace@gmail.com>
2023-11-10 05:20:43 +0800

    Add recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/create'
+  get 'recipes/update'
+  get 'recipes/edit'
+  get 'recipes/delete'
   root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
